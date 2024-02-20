fd8e0ab23605fac12d4bb8f55f68710a5694147f
ionca <canuion01@gmail.com>
2024-02-20 16:40:25 +0200

    First commit

---
+++ src/test/java/com/example/sportnavigator/SportNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportnavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportNavigatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
