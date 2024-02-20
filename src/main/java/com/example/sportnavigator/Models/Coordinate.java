c749206b72c133b39925741957cf0cef09d50cca
ionca <canuion01@gmail.com>
2024-02-20 20:12:34 +0200

    User_model_ dependencies
    Added user model, updated
    dependencies - added hibernate validator

--- src/main/java/com/example/sportnavigator/Models/Coordinate.java
+++ src/main/java/com/example/sportnavigator/Models/Coordinate.java
@@ -1,4 +1,5 @@
 package com.example.sportnavigator.Models;
 
 public class Coordinate {
+    
 }
--- src/main/java/com/example/sportnavigator/Models/User.java
+++ src/main/java/com/example/sportnavigator/Models/User.java
@@ -1,4 +1,36 @@
 package com.example.sportnavigator.Models;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long id;
+
+    @Column(name = "first_name")
+    @NotNull(message = "first name should not be empty")
+    @Size(min = 2, max = 30, message = "First name should be between 2 and 30 characters")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotNull(message = "last name should not be empty")
+    @Size(min = 2, max = 30, message = "Last name should not be empty")
+    private String lastName;
+
+
+
 }
