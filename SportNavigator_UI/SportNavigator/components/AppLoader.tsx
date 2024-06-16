2e7bf356f10e9c2aa14bc72acdd2192de1fe650b
ionca <canuion01@gmail.com>
2024-06-16 13:11:13 +0300

    loading icon

--- SportNavigator_UI/SportNavigator/components/AppLoader.tsx
+++ SportNavigator_UI/SportNavigator/components/AppLoader.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import {View, StyleSheet, Image} from 'react-native';
 import LottieView from 'lottie-react-native';
 
 
 const AppLoader = () => {
+    const animation = useRef(null);
+    useEffect(() => {
+
+    }, []);
+
+
+
     return(
         <View style={[StyleSheet.absoluteFillObject, styles.container]}>
-            {/* <LottieView style={{flex: 1,   width: 200,height: 200,zIndex: 9999,}} 
-            source={require('../assets/images/loading.json')}
-                autoPlay={true} loop={true}/> */}
-                <Image  style={{zIndex: 9999, height: 200}} source={require('../assets/images/loading_.gif')}/>
+                <Image  style={{zIndex: 9999}} source={require('../assets/images/VZ1I25iKuQ.gif')}/>
         </View>
     )
 }
-
+//'../assets/images/loading.json'
 
 const styles = StyleSheet.create({
     container:{
