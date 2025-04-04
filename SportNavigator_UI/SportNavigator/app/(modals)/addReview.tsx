97fa99facb0cd94eaa0d81ce913dd1648542fc33
Candu34 <126687929+Candu34@users.noreply.github.com>
2025-04-04 11:29:29 +0300

    [FrontEnd] Add review post request

--- SportNavigator_UI/SportNavigator/app/(modals)/addReview.tsx
+++ SportNavigator_UI/SportNavigator/app/(modals)/addReview.tsx
@@ -4,24 +4,87 @@ import { View, TouchableOpacity, TextInput } from "react-native";
 import { FontAwesome } from "@expo/vector-icons"; 
 import { defaultStyles } from "@/constants/Styles";
 import { StyleSheet } from "react-native";
+import * as SecureStore from 'expo-secure-store';
+import { API_URL } from "@/constants/api_url";
+import axios from "axios";
+import { useRouter, useLocalSearchParams } from "expo-router";
+import { Alert } from "react-native";
 
 
 const Page = () => {
     const [rating, setRating] = useState(0);
     const [description, setDescription] = useState('');
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [userId, setUserId] = useState(null);
+
+    const router = useRouter();
+    const params = useLocalSearchParams();
+    const courtId = params.id;
+
+    useEffect(() => {
+            const fetchUserId = async () => {
+                try {
+                    const userData = await SecureStore.getItemAsync('user');
+                    if (!userData) {
+                        throw new Error("[addReview] User not found in storage");
+                    }
+    
+                    const user = JSON.parse(userData);
+                    if (!user.id) {
+                        throw new Error("[addReview] Invalid user data");
+                    }
+    
+                    setUserId(user.id);
+                } catch (error) {
+                    console.error("[addReview] Error fetching user ID:", error);
+                }
+            };
+    
+            fetchUserId();
+        }, []);
 
 
     const handleDescriptionChange = (text: string) => {
       setDescription(text);
   };
 
-  const handleSummit = () => {
+  const handleSummit = async () => {
     if (description.length === 0 && rating === 0) {
         setError(true);
         return;
-    }
+    } 
+    setError(false);
 
+    const reviewDTO = {
+        description: description, 
+        rating: rating,
+        userID: userId,
+        sportCourtID: courtId,
+    };
+    console.log("[addReview] ", reviewDTO);
+    setLoading(true);
+    try {
+      const response = await axios.post(`${API_URL}/reviews`, reviewDTO, {
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      });
+      setLoading(false);
+            if (response.status === 200) {
+                setDescription('');
+                setRating(0);
+                setError(false);
+                Alert.alert('Success');
+                router.navigate("/listing/" + courtId);
+            } else {
+                Alert.alert('Error', response.data.message);
+            }
+        } catch (error) {
+            setLoading(false);
+            console.error('Error:', error);
+            Alert.alert('Error', error.message);
+        }
   }
 
 
